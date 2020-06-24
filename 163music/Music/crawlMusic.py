a06eae7edf9de27436ea754216b4b4c7a4a737fe
Maohua Yang <maohuay@hotmail.com>
2020-06-24 17:17:11 +0800

    网易云音乐：爬取歌曲评论进行分析

---
+++ 163music/Music/crawlMusic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
+'''
+@Time    : 2020/6/19 12:49
+@Author  : MaohuaYang
+@Contact : maohuay@hotmail.com 
+@File    : crawlMusic.py
+@Software: PyCharm
+'''
+import os
+import sys
